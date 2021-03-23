d4d7858bde2ff59e4b1febbf476862da7fcb4df3
lenovo <lenovo@DESKTOP-BLIQA1L>
2021-03-23 02:58:00 -0700

    Add project files.

---
+++ FileRead/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace FileRead
+{
+    partial class fileRead
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRead = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblPlace = new System.Windows.Forms.Label();
+            this.lblPhno = new System.Windows.Forms.Label();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPlace = new System.Windows.Forms.TextBox();
+            this.txtPh = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(128, 50);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(138, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(203, 202);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(63, 23);
+            this.btnNext.TabIndex = 2;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtRead);
+            this.panel1.Controls.Add(this.lblEmail);
+            this.panel1.Controls.Add(this.lblPlace);
+            this.panel1.Controls.Add(this.lblPhno);
+            this.panel1.Controls.Add(this.btnBack);
+            this.panel1.Controls.Add(this.txtEmail);
+            this.panel1.Controls.Add(this.txtPlace);
+            this.panel1.Controls.Add(this.txtPh);
+            this.panel1.Controls.Add(this.txtName);
+            this.panel1.Controls.Add(this.btnNext);
+            this.panel1.Controls.Add(this.lblName);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(314, 254);
+            this.panel1.TabIndex = 3;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Read Text";
+            // 
+            // txtRead
+            // 
+            this.txtRead.Location = new System.Drawing.Point(128, 12);
+            this.txtRead.Name = "txtRead";
+            this.txtRead.Size = new System.Drawing.Size(138, 20);
+            this.txtRead.TabIndex = 4;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(44, 168);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(32, 13);
+            this.lblEmail.TabIndex = 9;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblPlace
+            // 
+            this.lblPlace.AutoSize = true;
+            this.lblPlace.Location = new System.Drawing.Point(44, 130);
+            this.lblPlace.Name = "lblPlace";
+            this.lblPlace.Size = new System.Drawing.Size(34, 13);
+            this.lblPlace.TabIndex = 8;
+            this.lblPlace.Text = "Place";
+            // 
+            // lblPhno
+            // 
+            this.lblPhno.AutoSize = true;
+            this.lblPhno.Location = new System.Drawing.Point(44, 94);
+            this.lblPhno.Name = "lblPhno";
+            this.lblPhno.Size = new System.Drawing.Size(32, 13);
+            this.lblPhno.TabIndex = 7;
+            this.lblPhno.Text = "Phno";
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(128, 202);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(59, 23);
+            this.btnBack.TabIndex = 6;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(128, 161);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(138, 20);
+            this.txtEmail.TabIndex = 5;
+            // 
+            // txtPlace
+            // 
+            this.txtPlace.Location = new System.Drawing.Point(128, 123);
+            this.txtPlace.Name = "txtPlace";
+            this.txtPlace.Size = new System.Drawing.Size(138, 20);
+            this.txtPlace.TabIndex = 4;
+            // 
+            // txtPh
+            // 
+            this.txtPh.Location = new System.Drawing.Point(128, 87);
+            this.txtPh.Name = "txtPh";
+            this.txtPh.Size = new System.Drawing.Size(138, 20);
+            this.txtPh.TabIndex = 3;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(44, 57);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name";
+            // 
+            // fileRead
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 254);
+            this.Controls.Add(this.panel1);
+            this.Name = "fileRead";
+            this.Text = "File Read Sample";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblPlace;
+        private System.Windows.Forms.Label lblPhno;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPlace;
+        private System.Windows.Forms.TextBox txtPh;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtRead;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FileRead/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace FileRead
+{
+    public partial class fileRead : Form
+    {
+        int seeker = 0;
+        public fileRead()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetData();
+        }
+
+        private void GetData()
+        {
+            string[] fileData = File.ReadAllLines(@"C:\Users\lenovo\source\repos\FileRead\FileRead\bin\Debug\myDetails.txt");
+            string[] parcedData = fileData[seeker].Split(',', '+', '#');
+            txtName.Text = parcedData[0];
+            txtPh.Text = parcedData[1];
+            txtPlace.Text = parcedData[2];
+            txtEmail.Text = parcedData[3];
+
+            this.Text = (seeker + 1)+ "/"+ fileData.Length;
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            seeker++;
+            GetData();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            seeker--;
+            GetData();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
